9ff4635379b46270028b63096c01be6b002912f7
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2021-02-12 18:05:25 +0100

    challenge completed

--- ProductOfDigitOfSum.java
+++ ProductOfDigitOfSum.java
@@ -3,7 +3,30 @@ public class ProductOfDigitOfSum {
         new ProductOfDigitOfSum().run();
     }
 
-    void run(){
+    int sumDigProd(int... numbers){
+        int sum = 0;
+        for(int number : numbers){
+            sum += number;
+        }
+        String theSum = String.valueOf(sum);
+        int index = 0;
+        int product = 1;
+        while(index < theSum.length()){
+            product *= Character.getNumericValue(theSum.charAt(index));
+            index ++;
+        }
         
+        if(product < 10){
+            return product;
+        }
+        else{
+            return sumDigProd(product);
+        }
+    }
+
+    void run(){
+        System.out.println(sumDigProd(1, 2, 3, 4, 5, 6));
+
+        System.out.println(sumDigProd(16,28));
     }
 }
